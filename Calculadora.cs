71d6608d0fde47b9862f10ad0f166639e6f68dd1
Simon <124474593+Gerardo-Rivera@users.noreply.github.com>
2024-07-27 18:02:43 -0600

    Agregar archivos de proyecto.

---
+++ Calculadora.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace CalculadoraApp
+{
+    public static class Calculadora
+    {
+        public static float CalcularMedia(float[] numeros)
+        {
+            float suma = 0;
+            foreach (float num in numeros)
+            {
+                suma += num;
+            }
+            return suma / numeros.Length;
+        }
+
+        public static float CalcularMediana(float[] numeros)
+        {
+            float[] copia = new float[numeros.Length];
+            Array.Copy(numeros, copia, numeros.Length);
+            Array.Sort(copia);
+
+            int mitad = copia.Length / 2;
+            if (copia.Length % 2 == 0)
+            {
+                return (copia[mitad - 1] + copia[mitad]) / 2;
+            }
+            else
+            {
+                return copia[mitad];
+            }
+        }
+
+        public static float CalcularModa(float[] numeros)
+        {
+            Array.Sort(numeros);
+            float moda = numeros[0];
+            int maxCount = 1, count = 1;
+
+            for (int i = 1; i < numeros.Length; i++)
+            {
+                if (numeros[i] == numeros[i - 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    count = 1;
+                }
+
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                    moda = numeros[i];
+                }
+            }
+
+            return moda;
+        }
+
+        public static float CalcularDesviacionEstandar(float[] numeros)
+        {
+            float media = CalcularMedia(numeros);
+            float sumaDesviacion = 0;
+
+            foreach (float num in numeros)
+            {
+                sumaDesviacion += (num - media) * (num - media);
+            }
+
+            return (float)Math.Sqrt(sumaDesviacion / numeros.Length);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿namespace CalculadoraApp
+{
+    internal class Program
+    {
+        private static float[] numeros;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                MostrarMenu();
+                int opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        PedirNumeros();
+                        break;
+                    case 2:
+                        MostrarResultado("La media es: ", Calculadora.CalcularMedia(numeros));
+                        break;
+                    case 3:
+                        MostrarResultado("La mediana es: ", Calculadora.CalcularMediana(numeros));
+                        break;
+                    case 4:
+                        MostrarResultado("La moda es: ", Calculadora.CalcularModa(numeros));
+                        break;
+                    case 5:
+                        MostrarResultado("La desviación estándar es: ", Calculadora.CalcularDesviacionEstandar(numeros));
+                        break;
+                    case 6:
+                        Console.WriteLine("Saliendo...");
+                        return;
+                    default:
+                        Console.WriteLine("Opción no válida, intenta de nuevo.");
+                        break;
+                }
+
+                Console.WriteLine("\nPresiona Enter para continuar...");
+                Console.ReadLine();
+            }
+        }
+
+        static void MostrarMenu()
+        {
+            Console.WriteLine("Menú de Funciones:");
+            Console.WriteLine("1. Ingresar números");
+            Console.WriteLine("2. Calcular y mostrar la media");
+            Console.WriteLine("3. Calcular y mostrar la mediana");
+            Console.WriteLine("4. Calcular y mostrar la moda");
+            Console.WriteLine("5. Calcular y mostrar la desviación estándar");
+            Console.WriteLine("6. Salir");
+            Console.Write("Selecciona una opción: ");
+        }
+
+        static void PedirNumeros()
+        {
+            Console.Write("¿Cuántos números deseas ingresar? ");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+            numeros = new float[cantidad];
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.Write($"Ingresa el número {i + 1}: ");
+                numeros[i] = Convert.ToSingle(Console.ReadLine());
+            }
+        }
+
+        static void MostrarResultado(string mensaje, float resultado)
+        {
+            Console.WriteLine($"{mensaje} {resultado}");
+        }
+    }
+}
